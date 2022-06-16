7e7f838dbc6ee54e5632abd924765488e4b218f4
Demon <kristyndcb@gmail.com>
2022-06-16 13:58:33 -0600

    server.js up and running w cookies and authorization

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
+const path = require("path");
 const express = require("express");
-const routes = require("./controllers/routes");
-// import sequelize connection
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers/routes"); //this may change if file structure changes
+
 const sequelize = require("./config/index");
+const helpers = require("./utils/helpers");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// Set up sessions with cookies
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    // Stored in milliseconds (86400 === 1 day)
+    maxAge: 86400,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
