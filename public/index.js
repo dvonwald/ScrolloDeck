635f83bb65994927aaf5e581f75fed8ed394ee73
Alex Hageman <ahageman27@gmail.com>
2022-06-20 13:19:27 -0600

    worked on api call

--- public/index.js
+++ public/index.js
@@ -1,7 +1,17 @@
-var typeSelect = document.getElementById("gameTypeSelect");
-var playerSelect = document.getElementById("playerNumSelect");
+const typeSelect = document.getElementById("gameTypeSelect");
+const playerSelect = document.getElementById("playerNumSelect");
+const submit = document.getElementById("submitButton") 
 
 console.log(typeSelect);
 console.log(typeSelect.option[0]);
 console.log(typeSelect.option[0].value);
 alert(typeSelect.option[0].value);
+
+function filter() {
+    fetch(`../../controllers/api/games/search/minNumberOfPlayers/${playerSelect.value}/gameType/${typeSelect.value}`)
+        .then(res => {
+            console.log(res.json())
+        })
+    }
+
+    submit.addEventListener("click", filter())
